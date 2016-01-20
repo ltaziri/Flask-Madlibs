4fe734d9fa021fb150fb1e7864c9c3489228c4a0
Hackbright Student <info@hackbrightacademy.com>
2016-01-19 17:57:19 -0800

    Madlibs exercise complete

---
+++ madlibs.py
@@ -0,0 +1,71 @@
+from random import choice
+
+from flask import Flask, render_template, request
+
+
+# "__name__" is a special Python variable for the name of the current module
+# Flask wants to know this to know what any imported things are relative to.
+app = Flask(__name__)
+
+AWESOMENESS = [
+    'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza', 'oh-so-not-meh',
+    'brilliant', 'ducky', 'coolio', 'incredible', 'wonderful', 'smashing', 'lovely']
+
+@app.route('/')
+def start_here():
+    """Homepage."""
+
+    return "Hi! This is the home page."
+
+
+@app.route('/hello')
+def say_hello():
+    """Save hello to user."""
+
+    return render_template("hello.html")
+
+
+@app.route('/greet')
+def greet_person():
+    """Greet user."""
+
+    player = request.args.get("person")
+    compliment = choice(AWESOMENESS)
+
+    return render_template("compliment.html",
+                           person=player,
+                           compliment=compliment)
+
+@app.route('/game')
+def show_game_form():
+    """Play game with user"""
+
+    user_response = request.args.get("game")
+
+    if user_response == "yes":
+        return render_template("game.html")
+    else:
+        return render_template("goodbye.html") 
+
+@app.route('/madlib')
+def show_madlib():
+    """Show madlib to user"""
+
+    color_input = request.args.get("color")
+    noun_input = request.args.get("noun")
+    adjective_input = request.args.get("adjective")
+    name_input = request.args.get("name")
+    weather_input = request.args.getlist("weather")
+    weather_string = " ".join(weather_input)
+    print weather_string
+    return render_template("madlib.html", 
+                            game_color=color_input, 
+                            game_noun=noun_input, 
+                            game_adjective=adjective_input,
+                            game_person=name_input,
+                            game_weather=weather_string)
+
+if __name__ == '__main__':
+    # debug=True gives us error messages in the browser and also "reloads" our web app
+    # if we change the code.
+    app.run(debug=True)
